32a51135570e3f7c4fa61322515c7487f3108531
DongYan <yandssg6@gmail.com>
2018-10-14 21:02:13 -0700

    First commit

---
+++ src/main/java/onlineShop/dao/CartDao.java
@@ -0,0 +1,15 @@
+
+package onlineShop.dao;
+
+import java.io.IOException;
+
+import onlineShop.model.Cart;
+
+public interface CartDao {
+
+    Cart getCartById(int CartId);
+    
+    Cart validate(int cartId) throws IOException;
+    
+    void update(Cart cart);
+}
